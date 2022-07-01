e7db60a0a47c62ccfdc094f3e472e37889242fcc
Nick <nick@segpay.com>
2022-07-01 20:52:23 +0300

    Push app context

--- run_server.py
+++ run_server.py
@@ -6,21 +6,23 @@ from database.database import db
 from config import APP_SECRET, APP_NAME, DB_CONNECTION_STRING, TELEGRAM_BOT_TOKEN
 
 
-server = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
-server.config['SECRET_KEY'] = APP_SECRET
-server.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION_STRING
-server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = APP_SECRET
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION_STRING
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(server)
+db.init_app(app)
+app.app_context().push()
 
-@server.route('/' + TELEGRAM_BOT_TOKEN, methods=['POST'])
+
+@app.route('/' + TELEGRAM_BOT_TOKEN, methods=['POST'])
 def get_message():
     bot.process_new_updates([types.Update.de_json(flask.request.stream.read().decode('utf-8'))])
     return '!', 200
 
 
-@server.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     bot.remove_webhook()
     bot.set_webhook(url='https://{}.herokuapp.com/{}'.format(APP_NAME, TELEGRAM_BOT_TOKEN))
@@ -28,4 +30,4 @@ def index():
 
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
