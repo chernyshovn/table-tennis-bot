ffc15b29321108a9e5f95dd6338b92cf1ba5c8e7
Nick <nick@segpay.com>
2022-07-02 11:26:36 +0300

    Fix SingleMatchMatchAdder

--- services/single_match_match_adder.py
+++ services/single_match_match_adder.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from database.models import Tournament, Match
+from app import app
 
 
 class SingleMatchMatchAdder:
@@ -7,13 +8,14 @@ class SingleMatchMatchAdder:
         self.__db = db
 
     def add(self, tournament_id: int):
-        tournament = Tournament.query.filter_by(id=tournament_id).first()
-        match = Match(
-            initiator_telegram_user_id=tournament.initiator_telegram_user_id,
-            team_1_id=tournament.teams[0].id,
-            team_2_id=tournament.teams[1].id,
-            start_date_time=datetime.utcnow()
-        )
-        tournament.matches.append(match)
-        self.__db.session.add(tournament)
-        self.__db.session.commit()
+        with app.app_context():
+            tournament = Tournament.query.filter_by(id=tournament_id).first()
+            match = Match(
+                initiator_telegram_user_id=tournament.initiator_telegram_user_id,
+                team_1_id=tournament.teams[0].id,
+                team_2_id=tournament.teams[1].id,
+                start_date_time=datetime.utcnow()
+            )
+            tournament.matches.append(match)
+            self.__db.session.add(tournament)
+            self.__db.session.commit()
